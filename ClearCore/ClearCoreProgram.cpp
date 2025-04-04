036842e79b5a59579a2245c6870476c1b8b7fd5b
Mikem369 <mikem369@gmail.com>
2025-04-04 12:49:42 -0400

    Update ClearCoreProgram.cpp

--- ClearCore/ClearCoreProgram.cpp
+++ ClearCore/ClearCoreProgram.cpp
@@ -1,7 +1,5 @@
 #include "ClearCore.h"
 
-// Specifies which motor to move.
-// Options are: ConnectorM0, ConnectorM1, ConnectorM2, or ConnectorM3.
 //Defines motors to their connectors
 #define motorA ConnectorM0
 #define motorB ConnectorM1
@@ -16,13 +14,14 @@
 #define SerialPort ConnectorUsb
 #define InputPort ConnectorUsb
 
+//For clearcore 2000 pulses per sec is equal to 1rpm output with 100:1 gearbox attached.
 // Define the velocity and acceleration limits to be used for each move
-int32_t velocityLimit = 10000; // pulses per sec
-int32_t accelerationLimit = 100000; // pulses per sec^2
+int32_t velocityLimit = 1000; // pulses per sec
+int32_t accelerationLimit = 10000; // pulses per sec^2
 
-// Declares our user-defined helper function, which is used to command moves to
-// the motor. The definition/implementation of this function is at the  bottom
-// of the example.
+int32_t MotorZvelocityLimit = 1000; // pulses per sec
+int32_t MotorZaccelerationLimit = 10000; // pulses per sec^2
+
 void MoveDistance(int32_t distance);
 void RunTest();
 void ReadVoltage(int count);
@@ -30,10 +29,16 @@ void MoveXaxis(int32_t distance);
 void MoveUpYaxis(int32_t distance);
 void MoveDownYaxis(int32_t distance);
 void MoveZaxis(int32_t distance);
+void RunXTest();
+void RunYTest();
+void RunZTest();
+float ReadXVoltage();
+float ReadYVoltage();
+float ReadZVoltage();
 
 int main() {
     // Sets the input clocking rate.
-    MotorMgr.MotorInputClocking(MotorManager::CLOCK_RATE_LOW);
+    MotorMgr.MotorInputClocking(MotorManager::CLOCK_RATE_NORMAL);
 
     // Sets all motor connectors into step and direction mode.
     MotorMgr.MotorModeSet(MotorManager::MOTOR_ALL,
@@ -42,21 +47,22 @@ int main() {
     // These lines may be uncommented to invert the output signals of the
     // Enable, Direction, and HLFB lines. Some motors may have input polarities
     // that are inverted from the ClearCore's polarity.
-    //motor.PolarityInvertSDEnable(true);
+    motorA.PolarityInvertSDEnable(true);
+    motorB.PolarityInvertSDEnable(true);
     //motor.PolarityInvertSDDirection(true);
     //motor.PolarityInvertSDHlfb(true);
 
     // Sets the maximum velocity for each move
     motorA.VelMax(velocityLimit);
 	motorB.VelMax(velocityLimit);
-	motorZ1.VelMax(velocityLimit);
-	motorZ2.VelMax(velocityLimit);
+	motorZ1.VelMax(MotorZvelocityLimit);
+	motorZ2.VelMax(MotorZvelocityLimit);
 
     // Set the maximum acceleration for each move
     motorA.AccelMax(accelerationLimit);
 	motorB.AccelMax(accelerationLimit);
-	motorZ1.AccelMax(accelerationLimit);
-	motorZ2.AccelMax(accelerationLimit);
+	motorZ1.AccelMax(MotorZaccelerationLimit);
+	motorZ2.AccelMax(MotorZaccelerationLimit);
 
     // Sets up serial communication and waits up to 5 seconds for a port to open.
     // Serial communication is not required for this example to run.
@@ -86,18 +92,202 @@ int main() {
     //    continue;
     //}
 	int16_t input=0;
+
 	while(true){
 			
 		input = InputPort.CharGet();
-		while((char)input=='1'){
+		if((char)input=='1'){
 			//Starts tests that moves motors
 			RunTest();
-			
 			//Test finishes
 			SerialPort.SendLine("0");
 			input = 0;
 		}
+		
+		if((char)input=='x'){
+			RunXTest();
+			SerialPort.SendLine("0");
+			input = 0;	
+		}
+		
+		if((char)input=='y'){
+			RunYTest();
+			SerialPort.SendLine("0");
+			input = 0;
+		}
+		
+		if((char)input=='z'){
+			RunZTest();
+			SerialPort.SendLine("0");
+			input = 0;
+		}
     }
+}
+
+void RunXTest(){
+	
+	MoveXaxis(200);
+	MoveXaxis(-200);
+}
+void RunYTest(){
+	
+	MoveUpYaxis(200);
+	MoveDownYaxis(-200);
+}
+
+void RunZTest(){
+	
+	MoveZaxis(200);
+	MoveZaxis(-200);
+}
+
+
+void TestSensor(){
+		float xD,yD,zD;
+		xD = ReadXVoltage();
+		yD = ReadYVoltage();
+		zD = ReadZVoltage();
+			
+		while(xD!=2.15 && yD!=2.00){
+				
+			if(yD < 2.00){
+				MoveUpYaxis(1);
+			}
+			else if(yD > 2.00){
+				MoveDownYaxis(1);
+			}
+				
+			if(xD < 2.15){
+				MoveXaxis(1);
+			}
+			else if(xD > 2.15){
+				MoveXaxis(-1);
+			}
+			
+			xD = ReadXVoltage();
+			yD = ReadYVoltage();			
+				
+		}
+		
+		ReadVoltage(1);
+		while(xD!=1.85 && yD!=2.00){
+				
+			if(yD < 2.00){
+				MoveUpYaxis(1);
+			}
+			else if(yD > 2.00){
+				MoveDownYaxis(1);
+			}
+				
+			if(xD < 1.85){
+				MoveXaxis(1);
+			}
+			else if(xD > 1.85){
+				MoveXaxis(-1);
+			}
+			
+			xD = ReadXVoltage();
+			yD = ReadYVoltage();
+		}
+			
+		ReadVoltage(2);
+		while(xD!=2.00 && yD!=2.15){
+
+			if(xD < 2.00){
+				MoveXaxis(1);
+			}
+			else if(xD > 2.00){
+				MoveXaxis(-1);
+			}
+			if(yD < 2.15){
+				MoveUpYaxis(1);
+			}
+			else if(yD > 2.15){
+				MoveDownYaxis(1);
+			}
+			
+			xD = ReadXVoltage();
+			yD = ReadYVoltage();
+		}
+			
+		ReadVoltage(3);
+			
+		while(xD!=2.00 && yD!=1.85){
+
+			if(xD < 2.00){
+				MoveXaxis(1);
+			}
+			else if(xD > 2.00){
+				MoveXaxis(-1);
+			}
+			if(yD < 1.85){
+				MoveUpYaxis(1);
+			}
+			else if(yD > 1.85){
+				MoveDownYaxis(1);
+			}
+			xD = ReadXVoltage();
+			yD = ReadYVoltage();
+			
+		}
+			
+		ReadVoltage(4);
+		while(xD!=2.00 && yD!=1.85 && zD!=1.85){
+
+			if(xD < 2.00){
+				MoveXaxis(1);
+			}
+			else if(xD > 2.00){
+				MoveXaxis(-1);
+			}
+			
+			if(yD < 1.85){
+				MoveUpYaxis(1);
+			}
+			else if(yD > 1.85){
+				MoveDownYaxis(1);
+			}
+			
+			if(zD < 1.85){
+				MoveZaxis(1);
+			}
+			else if(zD > 1.85){
+				MoveZaxis(-1);
+			}
+			xD = ReadXVoltage();
+			yD = ReadYVoltage();
+			zD = ReadZVoltage();
+		}
+		
+		ReadVoltage(5);
+		while(xD!=2.00 && yD!=1.85 && zD!=2.15){
+
+			if(xD < 2.00){
+				MoveXaxis(1);
+			}
+			else if(xD > 2.00){
+				MoveXaxis(-1);
+			}
+			
+			if(yD < 1.85){
+				MoveUpYaxis(1);
+			}
+			else if(yD > 1.85){
+				MoveDownYaxis(1);
+			}
+			
+			if(zD < 2.15){
+				MoveZaxis(1);
+			}
+			else if(zD > 2.15){
+				MoveZaxis(-1);
+			}
+			xD = ReadXVoltage();
+			yD = ReadYVoltage();
+			zD = ReadZVoltage();
+		}
+		ReadVoltage(6);
+		
 }
 
 void RunTest(){
@@ -105,7 +295,7 @@ void RunTest(){
 	ReadVoltage(0);
 			
 	//X-axis moves to one extreme
-	MoveXaxis(100);
+	MoveXaxis(18000);
 
 	//Reads voltage of sensor
 	Delay_ms(1000);
@@ -113,7 +303,7 @@ void RunTest(){
 	Delay_ms(1000);
 			
 	//X-axis moves to other extreme
-	MoveXaxis(-200);
+	MoveXaxis(-2000);
 
 	//Reads voltage of sensor
 	Delay_ms(1000);
@@ -124,7 +314,7 @@ void RunTest(){
 	MoveXaxis(100);
 			
 	//Y-axis moves to one extreme
-	MoveUpYaxis(100);
+	MoveUpYaxis(10000);
 
 	//Reads voltage of sensor
 	Delay_ms(1000);
@@ -160,6 +350,49 @@ void RunTest(){
 			
 	//Centers z-axis
 	MoveZaxis(150);	
+}
+
+float ReadXVoltage(){
+		int16_t xResult;
+		xResult = ConnectorA12.State();
+		
+		// Convert the reading to a voltage.
+		double xVoltage;
+		xVoltage = 10.0 * xResult / ((1 << adcResolution) - 1);
+		
+		double Xmm;
+		Xmm = xVoltage * 0.35;
+		
+		return Xmm;
+}
+
+float ReadYVoltage(){
+		int16_t yResult;
+		yResult = ConnectorA11.State();
+		
+		// Convert the reading to a voltage.
+		double yVoltage;
+		yVoltage = 10.0 * yResult / ((1 << adcResolution) - 1);
+
+		double Ymm;
+		Ymm = yVoltage * 0.35;
+		
+		return Ymm;
+
+}
+
+float ReadZVoltage(){
+		int16_t zResult;
+		zResult = ConnectorA10.State();
+		
+		// Convert the reading to a voltage.
+		double zVoltage;
+		zVoltage = 10.0 * zResult / ((1 << adcResolution) - 1);
+		
+		double Zmm;
+		Zmm = zVoltage * 0.35;
+		
+		return Zmm;
 }
 
 void ReadVoltage(int count){
@@ -198,8 +431,6 @@ void MoveXaxis(int32_t distance){
 	while (!motorA.StepsComplete() && !motorB.StepsComplete()) {
 		continue;
 	}
-	
-	Delay_ms(200);
 }
 
 //Moves sensor ring up on Y-axis
@@ -210,7 +441,6 @@ void MoveUpYaxis(int32_t distance){
 	while (!motorA.StepsComplete() && !motorB.StepsComplete()) {
 		continue;
 	}
-	Delay_ms(200);
 }
 
 //Moves sensor ring down on Y-axis
@@ -221,7 +451,6 @@ void MoveDownYaxis(int32_t distance){
 	while (!motorA.StepsComplete() && !motorB.StepsComplete()) {
 		continue;
 	}
-	Delay_ms(200);
 }
 
 //Moves shaft up or down(use negative value to change direction)
@@ -232,7 +461,6 @@ void MoveZaxis(int32_t distance){
 	while (!motorZ1.StepsComplete() && !motorZ2.StepsComplete()) {
 		continue;
 	}
-	Delay_ms(200);
 }
 
 /*------------------------------------------------------------------------------
